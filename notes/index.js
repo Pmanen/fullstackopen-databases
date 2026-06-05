0c80a3efbca947e97da6187a1565d41be3005810
Pippa van Manen <pippavanmanen@icloud.com>
2026-06-05 18:09:01 +0200

    ex 4

--- notes/index.js
+++ notes/index.js
@@ -13,30 +13,37 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     : {},
 });
 
-class Note extends Model {}
-Note.init(
+class Blog extends Model {}
+Blog.init(
   {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
     },
-    content: {
+    title: {
       type: DataTypes.TEXT,
       allowNull: false,
     },
-    important: {
-      type: DataTypes.BOOLEAN,
+    author: {
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
-    date: {
-      type: DataTypes.DATE,
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
     },
   },
   {
     sequelize,
     underscored: true,
     timestamps: false,
-    modelName: "note",
+    modelName: "blog",
   },
 );
 
@@ -45,13 +52,26 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.get("/api/notes", async (req, res) => {
-  const notes = await Note.findAll();
-  res.json(notes);
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
 });
 
-app.post("/api/notes", async (req, res) => {
+app.post("/api/blogs", async (req, res) => {
   console.log(req.body);
-  const note = await Note.create({ ...req.body, date: new Date() });
-  res.json(note);
+  try {
+    const blog = await Blog.create({ ...req.body });
+    res.json(blog);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  const { id } = req.params;
+  const deleted = await Blog.destroy({ where: { id } });
+  if (!deleted) {
+    return res.status(404).end();
+  }
+  res.status(204).end();
 });
