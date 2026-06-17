2fd1df35f3312ddecead9162159131fd89e879ef
Pippa van Manen <pippavanmanen@icloud.com>
2026-06-17 16:58:51 +0200

    ex 5 better structure

--- notes/index.js
+++ notes/index.js
@@ -1,77 +1,20 @@
-require("dotenv").config();
-const { Sequelize, Model, DataTypes } = require("sequelize");
-const express = require("express");
-const app = express();
-app.use(express.json());
+const express = require('express')
+const app = express()
 
-const isProduction = process.env.NODE_ENV === "production";
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  dialectOptions: isProduction
-    ? { ssl: { require: true, rejectUnauthorized: false } }
-    : {},
-});
+const blogsRouter = require('./controllers/blogs')
 
-class Blog extends Model {}
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    title: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    author: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    url: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "blog",
-  },
-);
+app.use(express.json())
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.use('/api/blogs', blogsRouter)
 
-app.get("/api/blogs", async (req, res) => {
-  const blogs = await Blog.findAll();
-  res.json(blogs);
-});
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
 
-app.post("/api/blogs", async (req, res) => {
-  console.log(req.body);
-  try {
-    const blog = await Blog.create({ ...req.body });
-    res.json(blog);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-app.delete("/api/blogs/:id", async (req, res) => {
-  const { id } = req.params;
-  const deleted = await Blog.destroy({ where: { id } });
-  if (!deleted) {
-    return res.status(404).end();
-  }
-  res.status(204).end();
-});
+start()
