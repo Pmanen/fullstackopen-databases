27bcb0268b4c134bf17cea59f20c83e3f846431a
Pippa van Manen <pippavanmanen@icloud.com>
2026-06-18 23:39:58 +0200

    ex 10 blog ownership

--- notes/controllers/blogs.js
+++ notes/controllers/blogs.js
@@ -1,6 +1,22 @@
 const router = require('express').Router()
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../util/config')
 
-const { Blog } = require('../models')
+const { Blog, User } = require('../models')
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch{
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  }  else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
 
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll({
@@ -13,9 +29,10 @@ router.get('/', async (req, res) => {
   res.json(blogs)
 })
 
-router.post('/', async (req, res, next) => {
+router.post('/', tokenExtractor, async (req, res, next) => {
   try {
-    const blog = await Blog.create({... req.body }) // TODO UPDATE
+    const user = await User.findByPk(req.decodedToken.id)
+    const blog = await Blog.create({... req.body, userId: user.id })
     res.json(blog)
   } catch(error) {
     next(error)
