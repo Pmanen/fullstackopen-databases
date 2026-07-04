3c4ecdb8129a1e1523098645749992c755ea838a
Pippa van Manen <pippavanmanen@icloud.com>
2026-07-04 14:29:58 +0200

    ex 22 bookkeeping

--- notes/models/index.js
+++ notes/models/index.js
@@ -7,6 +7,10 @@ Blog.belongsTo(User)
 
 User.belongsToMany(Blog, { through: ReadingList, as: 'readings' })
 Blog.belongsToMany(User, { through: ReadingList, as: 'saved_by' })
+User.hasMany(ReadingList, { as: 'reading_list' })
+ReadingList.belongsTo(User)
+Blog.hasMany(ReadingList, { as: 'reading_list' })
+ReadingList.belongsTo(Blog)
 
 
 module.exports = {
